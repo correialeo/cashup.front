bb0b4e2f2802487f7b80590917cd9bb6fb1d0f25
correialeo <leandroo774@outlook.com>
2025-04-01 12:01:45 -0300

    feat: cadastro de categorias implementado com integraçao backend

--- src/actions/category-actions.ts
+++ src/actions/category-actions.ts
@@ -1,8 +1,39 @@
-import { Category } from "@/app/types";
+import { redirect } from "next/navigation";
 
 const API_URL="http://localhost:8080/api/category"
 
 export async function getCat(){
     const response = await fetch(API_URL);
     return await response.json();
+}
+
+export async function createCat(initialState: any, cat: FormData){
+    const body = {
+        name: cat.get('name'),
+        icon: cat.get('icon'),
+    }
+
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+    });
+
+    if (!response.ok){
+        const errors = await response.json();
+        return{
+            values: {
+                name: cat.get('name'),
+                icon: cat.get('icon'),
+            },
+            errors: {
+                name: errors.find(error => error.field === 'name')?.message,
+                icon: errors.find(error => error.field === 'icon')?.message,
+            }
+        }
+    }
+
+    redirect("/categories")
 }
--- src/app/categories/form/page.tsx
+++ src/app/categories/form/page.tsx
@@ -1,10 +1,32 @@
+"use client";
+
+import { createCat } from "@/actions/category-actions";
 import NavBar from "@/components/Header/nav-bar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ArrowLeft, Check } from "lucide-react";
 import Link from "next/link";
+import { useActionState } from "react"; 
+
+const initialState = {
+    values: {
+        name: "",
+        icon: "",
+    },
+    errors: {
+        name: "",
+        icon: "",
+    }
+}
 
 export default function CategoryFormPage(){
+    const [state, formAction, pending] = useActionState(
+        async (prevState: typeof initialState, formData: FormData) => {
+            return await createCat(prevState, formData);
+        },
+        initialState
+    );
+
     return(
         <>
             <NavBar active="Categories"/>
@@ -12,19 +34,40 @@ export default function CategoryFormPage(){
             <main className="flex justify-center">
                 <div className="bg-slate-900 min-w-2/3 m-6 p-6 rounded">
                     <h2 className="text-lg font-semibold">Cadastrar Categoria</h2>
-                    <form action="">
-                        <Input placeholder="Nome da Categorie" name="name"/>
-                        <Input placeholder="Nome do ícone" name="icon"/>
+                    <form action={formAction}>
+                        <div>
+                            <Input 
+                                placeholder="Nome da Categoria" 
+                                name="name" 
+                                aria-invalid={!!state?.errors.name}
+                                defaultValue={state?.values.name} 
+                            />
+                            <span className="text-sm text-destructive">
+                                {state?.errors.name}
+                            </span>
+                        </div>
+
+                        <div>
+                            <Input 
+                                placeholder="Nome do ícone" 
+                                name="icon" 
+                                aria-invalid={!!state?.errors.icon}
+                                defaultValue={state?.values.icon}
+                            />
+                            <span className="text-sm text-destructive">
+                                {state?.errors.icon}
+                            </span>
+                        </div>
 
                         <div className="flex justify-end gap-2 mt-4">
                             <Button asChild variant="outline">
                                 <Link href="/categories">
-                                    <ArrowLeft/>Cancelar
+                                    <ArrowLeft className="mr-2 h-4 w-4"/>Cancelar
                                 </Link>
                             </Button>
-                            <Button>
-                                <Check/>
-                                Cadastrar
+                            <Button disabled={pending}>
+                                <Check className="mr-2 h-4 w-4"/>
+                                {pending ? "Enviando..." : "Cadastrar"}
                             </Button>
                         </div>
                     </form>

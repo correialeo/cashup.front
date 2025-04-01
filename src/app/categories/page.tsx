f3e0693678644d59c63ac236a985a5b25a377a9d
correialeo <leandroo774@outlook.com>
2025-04-01 10:27:46 -0300

    feat: botao para criar categorias colocado na interface

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,6 +1,9 @@
 import CategoryItem from "@/components/Category/category-item";
 import NavBar from "@/components/Header/nav-bar";
 import { Category } from "../types";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import Link from "next/link";
 
 async function getCat(){
     const response = await fetch("http://localhost:8080/api/category");
@@ -16,7 +19,16 @@ export default async function CategoriesPage(){
 
             <main className="flex justify-center">
                 <div className="bg-slate-900 min-w-2/3 m-6 p-6 rounded">
-                    <h2 className="text-lg font-semibold">Categorias</h2>
+                    <div className="flex justify-between items-center mb-4">
+                        <h2 className="text-lg font-semibold">Categorias</h2>
+                        <Button asChild>
+                            <Link href="/categories/form">
+                                <Plus/>
+                                Nova Categoria
+                            </Link>
+                        </Button>
+                    </div>
+
                     {data.length == 0 ? <p>Categoria nao cadastrada</p> :
                     data.map(category => <CategoryItem key={category.id} category={category}/>)}
                 </div>
